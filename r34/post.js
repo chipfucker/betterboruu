6a50c1c79d825c3483b9da1e5f26ce6fe8ecae0c
Marlie <lizzpegschris@gmail.com>
2025-05-14 12:46:22 -0500

    turn off debug for post.js

--- r34/post.js
+++ r34/post.js
@@ -15,7 +15,7 @@ window.onerror = function handleError(e, url, line) {
     return false;
 };
 
-const debug = true;
+const debug = false;
 const debugErrMsg = "Debug: Forced error";
 const debugPosts = {
     link: {
